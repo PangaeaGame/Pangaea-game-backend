eb2a71f224a94c5a822ba52d698d0a673c6b71cc
Fredpwol <fredpwol@gmail.com>
2022-04-13 20:50:39 +0100

    rename model

--- maps/models.py
+++ maps/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Maps(models.Model):
+class Map(models.Model):
     coordX = models.IntegerField(null=False)
     coordY = models.IntegerField(null=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
